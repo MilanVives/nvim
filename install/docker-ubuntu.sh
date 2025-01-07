bfbba1c1be7e625a191828ea8b50b1c1ce2bc8df
M. Dima <milan.dima@vives.be>
2025-01-07 16:29:31 +0100

    Update docker-ubuntu.sh

--- install/docker-ubuntu.sh
+++ install/docker-ubuntu.sh
@@ -17,3 +17,8 @@ echo \
 sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+
+ sudo groupadd docker
+ sudo usermod -aG docker $USER
+ newgrp docker
